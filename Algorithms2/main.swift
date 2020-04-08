ed8f0bace5123fe8873ad23aaf1b097685c5d315
Adam Salata <adamsalata@gmail.com>
2020-04-08 22:50:24 +0200

    Initial Commit

---
+++ Algorithms2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms2
+//
+//  Created by Adam on 08/04/2020.
+//  Copyright © 2020 lettucesoftware. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
